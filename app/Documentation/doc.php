28cfabfeec6883b853292028d79be792ccb34436
Cristian Rojas <ingsis2@cenicana.org>
2020-02-05 15:23:33 -0500

    se añade swagger para tener una UI del API

---
+++ app/Documentation/doc.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *       
+ *  @OA\Info(
+ *     description="Servicio que permite consultar y ejecutar procesos de fabrica",
+ *     version="1.0.0",
+ *     title="API Ceniprof",
+ *     @OA\Contact(
+ *         name="SETI",
+ *         email="meteorlogia@cenicana.org"
+ *     )
+  *  )
+ * 
+ */
+/**
+ * @OA\Tag(
+ *     name="Autenticación",
+ *     description="Operaciones para el proceso de autenticación",
+ * )
+ */
